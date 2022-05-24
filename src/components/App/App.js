ab34b73eb7bd8b1d2d0e670ef88663f781b61975
Adam Nyberg <adamnyberg95@gmail.com>
2022-05-24 12:00:01 +0200

    tuesday lesson- not done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,143 +1,184 @@
 import "./App.css";
-import SideNav from "../SideNav/SideNav";
-import Playlist from "../Playlist/Playlist";
-import MobileNav from "../MobileNav/MobileNav";
+import SideNav from "../SideNav/SideNav.js";
+import Playlist from "../Playlist/Playlist.js";
 import { Box } from "@mui/material";
-import Player from "../Player/Player";
+import MobilNav from "../MobileNav/MobileNav";
 import { Routes, Route } from "react-router-dom";
+import Player from "../Player/Player";
 import Library from "../Library/Library";
 import Home from "../Home/Home";
+import Login from "../Login/Login";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import SpotifyWebApi from "spotify-web-api-node";
+import { fetchUser, fetchPlaylist } from "../../store/actions/index";
 
-function App() {
-  const mockData = [
-    { name: "Rock", playlistId: 123, image: "/Justin-Bieber.png" },
-    { name: "Pop", playlistId: 646, image: "/Justin-Bieber.png" },
-    { name: "Hip hop", playlistId: 834, image: "/Justin-Bieber.png" },
-    { name: "X-mas", playlistId: 5503, image: "/Justin-Bieber.png" },
-    { name: "Code life", playlistId: 4832, image: "/Justin-Bieber.png" },
-  ];
-  const songs = [
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-    {
-      image: "/Justin-Bieber.png",
-      title: "Holy",
-      artist: "Justin Bieber",
-      album: "No clue",
-      duration: 180,
-    },
-  ];
+const mockData = [
+  { name: "Rock", playlistId: 123, image: "/Justin-Bieber.png" },
+  { name: "Pop", playlistId: 646, image: "/Justin-Bieber.png" },
+  { name: "Hip hop", playlistId: 834, image: "/Justin-Bieber.png" },
+  { name: "X-mas", playlistId: 5503, image: "/Justin-Bieber.png" },
+  { name: "Code life", playlistId: 4832, image: "/Justin-Bieber.png" },
+];
+
+const songs = [
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 155,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 162,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 181,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+  {
+    image: "/Justin-Bieber.png",
+    title: "Holy",
+    artist: "Justin Bieber",
+    album: "No clue",
+    duration: 180,
+  },
+];
+
+function App({ token, fetchUser, fetchPlaylist }) {
+  const spotifyApi = new SpotifyWebApi();
+
+  useEffect(() => {
+    spotifyApi.setAccessToken(token);
+
+    const getData = async () => {
+      fetchUser(spotifyApi);
+      fetchPlaylist(spotifyApi);
+    };
+
+    if (token) getData();
+  }, [token, fetchUser]);
+
   return (
-    <div className="App">
-      <Box
-        sx={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+    <Box className="App">
+      {token ? (
         <Box
-          sx={{
-            flex: 1,
-            display: "flex",
-            overflowY: "auto",
-          }}
-        >
-          <SideNav playlists={mockData} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/search"
-              element={<h1 style={{ color: "white" }}>Search</h1>}
-            />
-            <Route path="/library" element={<Library playlists={mockData} />} />
-            <Route
-              path="/playlist/:playlistId"
-              element={<Playlist songs={songs} />}
-            />
-          </Routes>
-        </Box>
-        <Player />
-        <MobileNav />
-        <Box
-          px={3}
           sx={{
             width: "100%",
-            height: 25,
-            backgroundColor: "primary.main",
+            height: "100%",
             display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            fontSize: 14,
-            boxSizing: "border-box",
+            flexDirection: "column",
           }}
         >
-          Made with ❤️ by Techover Academy 👨‍💻
+          <Box sx={{ flex: 1, overflowY: "auto", display: "flex" }}>
+            <SideNav playlists={mockData} />
+            <Routes>
+              <Route
+                path="/playlist/:id"
+                element={<Playlist songs={songs} />}
+              />
+              <Route
+                path="/search"
+                element={<h1 style={{ color: "white" }}>Search</h1>}
+              />
+              <Route
+                path="/library"
+                element={<Library playlists={mockData} loading={false} />}
+              />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Box>
+          <Player />
+          <MobilNav />
+          <Banner />
         </Box>
-      </Box>
-    </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      )}
+    </Box>
   );
 }
-export default App;
+
+const Banner = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 25,
+        bgcolor: "primary.main",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        fontSize: 14,
+        boxSizing: "border-box",
+        paddingRight: "10px",
+      }}
+    >
+      Made with love by Techover Academy
+    </Box>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { token: state.auth.token };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchUser: (api) => dispatch(fetchUser(api)),
+    fetchPlaylist: (api) => dispatch(fetchPlaylist(api)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatch)(App);
